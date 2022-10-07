ca8f8945f207be5abb395ab9514373300f1a7f03
Yelena Cox <yelenacox@gmail.com>
2022-10-07 10:05:14 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const quarterValue = (scaryNumber) => {
+const quarter = scaryNumber / 4
+return quarter
+
+}
+
+// Invoke the function and store the return value
+const returnedNumber = quarterValue(38)
+
+// Log the return value
+console.log(returnedNumber)
